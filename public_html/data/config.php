4d33899a477a0c2f8c688a78b409de4a23f20039
fjwcoder <fjwcoder@gmail.com>
2018-04-26 17:40:18 +0800

    ECTouch微分销修改：创建

---
+++ public_html/data/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'DB_TYPE'   => 'mysql',
+    'DB_HOST'   => '47.93.17.8',
+    'DB_USER'   => 'root',
+    'DB_PWD'   => 'qiandaozg',
+    'DB_NAME'   => 'ecs_wfx',
+    'DB_PREFIX'   => 'ecs_',
+    'DB_PORT'   => '3306',
+    'DB_CHARSET'   => 'utf8',
+);
